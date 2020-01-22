0d7fef52a81b3a3043b017b5ffbec1627bb2a9a4
TascuNicoleta <parlogeanicoleta@yahoo.com>
2020-01-22 13:19:21 +0200

    create client proj

---
+++ Client/Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Client
+//
+//  Created by Nicoleta Tascu on 22/01/2020.
+//  Copyright © 2020 Nicoleta Tascu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
